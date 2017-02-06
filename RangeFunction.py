1a83a7fe3a48da1b057c13ce6af1ae43578cdb84
lilychau <lily.chau28@gmail.com>
2017-02-06 10:32:54 -0800

    first

--- RangeFunction.py
+++ RangeFunction.py
@@ -1,5 +1,7 @@
 """Use the range function to print the numbers from 40 to 1 (backwards)"""
-
+#i used the number 41 so that it would begin with the number 40
+for i in reversed(range(1, 41)):
+    print(i)
 
 
 """Repeat the exercise but count by 5's"""
