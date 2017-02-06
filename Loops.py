a8ab8c7c868710166b316bd365c3d3f96dbf92ef
lilychau <lily.chau28@gmail.com>
2017-02-06 10:32:07 -0800

    finished

--- Loops.py
+++ Loops.py
@@ -16,3 +16,15 @@ print(FiveLoopFunc(f))
 """Write a program that will prompt the user for an input value (n) and double
 it IF is an ODD number, triple it if is an EVEN number and do nothing if it is
 anything else (like a decimal or a string)"""
+#if you laughed then i should get E.C. for jokes
+n = int(input("Give me a number under 100. 'Even' if the 'Odds' aren't in your favor: "))
+
+def EvenOddFunc(n):
+    if n%2 == 0 :
+        n = n * 3
+    elif n%2 != 0 :
+        n = n * 2
+    else:
+        print("Okay, i asked for a number...")
+
+print(EvenOddFunc(n))
