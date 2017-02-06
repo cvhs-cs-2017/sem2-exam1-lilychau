25081755c7fb6f65facae7a8897c0dcda9264349
lilychau <lily.chau28@gmail.com>
2017-02-06 10:34:22 -0800

    first

--- Turtle.py
+++ Turtle.py
@@ -1,4 +1,27 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
+import turtle
+lily = turtle.Turtle()
+
+n = int(input("Put in a number, and ill make a cube: "))
+def ThreeDCubeFunc(lily, n):
+    for i in range(4):
+        lily.forward(n)
+        lily.right(90)
+    lily.left(45)
+    lily.forward(n)
+    lily.right(45)
+    lily.forward(n)
+    lily.right(135)
+    lily.forward(n)
+    lily.left(45)
+    lily.forward(n)
+    lily.left(135)
+    lily.forward(n)
+    lily.left(45)
+    lily.forward(n)
+
+ThreeDCubeFunc(lily, n)
+input()
 
 
 
