5290afe051ff91bea48afd1dc6f81ac785ee6dae
lilychau <lily.chau28@gmail.com>
2017-02-06 10:33:18 -0800

    second

--- RangeFunction.py
+++ RangeFunction.py
@@ -5,7 +5,8 @@ for i in reversed(range(1, 41)):
 
 
 """Repeat the exercise but count by 5's"""
-
+for i in reversed(range(0,45,5)):
+    print(i)
 
 
 """Write a program that will count print all the multiples of (n) where n is
